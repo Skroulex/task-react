901868f823e683ce45a8d99f4e56904e2ae000a8
Skroulex <eshimbekov91@gmail.com>
2022-06-17 14:43:42 +0600

    task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header/Header.js";
+import Section from "./Section/Section.js";
+import Footer from "./Footer/Footer.js";
+import Models from "./Models/Models";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    let users = [
+        {
+            name: "Dzhalil",
+            lastName: "Eshimbekov",
+            age: 21,
+            id: 1,
+        },
+        {
+            name: "Ilim",
+            lastName: "Kushbekov",
+            age: 20,
+            id: 2,
+        },
+    ];
+
+    return (
+        <div>
+            <Header />
+            <Section users={users} />
+            <Models />
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
